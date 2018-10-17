f9e4d7f7f704e120bf10d877c7a1b3f2055c3e74
Charles-Tam <tamcheuklai@outlook.com>
2018-10-17 23:51:35 +0800

    Q1, Q6 done

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -1,10 +1,41 @@
 package answers;
 
 public class Question6 {
-
+	public static int numServerst;
+	public static int targetServert;
+	public static int[][] timeso;
+	public static int[] minTime;
+	
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
 		// TODO Auto-generated method stub
-		return -1;
+		numServerst = numServers;
+		targetServert = targetServer;
+		timeso = new int[numServers][numServers];
+		minTime = new int[numServers];
+		
+		minTime[targetServert] = times[0][targetServert];
+		for(int i=0;i<numServerst;i++){
+			minTime[i]=times[0][i];
+		}
+		for(int i=0;i<numServerst;i++){
+			for(int q=0;q<numServerst;q++){
+				timeso[i][q] = times[i][q];
+			}
+		}
+		Question6.recursive(0); //calling recursive
+		return minTime[targetServert];
+	}
+	
+	
+	public static void recursive(int Server){
+	 for(int i=1;i<numServerst;i++){
+		 if(Server==i){
+		 }else if(Server==targetServert){
+		 }else if((minTime[Server]+timeso[Server][i])<=minTime[i]){
+			 minTime[i] = minTime[Server]+timeso[Server][i];
+			 Question6.recursive(i);
+		 }
+	 }
 	}
 
 }
