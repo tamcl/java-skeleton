1fd41bb824589da2791ea2a9d5842385d0320884
Charles-Tam <tamcheuklai@outlook.com>
2018-10-18 23:42:20 +0800

    Q4 done

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -4,7 +4,16 @@ public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
 		// TODO Auto-generated method stub
-		return -1;
+		int output=0, Machines;
+		if(numberMachines<=rows.length){
+			Machines = rows[numberMachines].length;
+			for(int i=0; i<Machines;i++){
+				if(rows[numberMachines][i].equals("X")==false){
+					output += Integer.parseInt(rows[numberMachines][i]);
+				}
+			}
+		}
+		return output;
 	}
 
 }
