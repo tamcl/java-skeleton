c06f366404ae517e28edc44f0efb4e4aa6de393c
Charles-Tam <tamcheuklai@outlook.com>
2018-10-19 05:10:33 +0800

    edit q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -4,14 +4,13 @@ public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 		// TODO Auto-generated method stub
-		int count = 0, sndcount = 0, size = 0;
-		int max=0, current=0;
-		size = portfolios.length;
-		for(count=0;count<size-1;count++){
-			for(sndcount=count+1;sndcount<size;sndcount++){
-				current = portfolios[count]^portfolios[sndcount];
+		int max=0,current =0;
+		int size = portfolios.length;
+		for(int count=0;count<size-1;count++){
+			for(int sndcount=count+1;sndcount<size;sndcount++){
+				current=portfolios[count]^portfolios[sndcount];
 				if(current>max){
-					max=current;
+					max=portfolios[count]^portfolios[sndcount];
 				}
 			}
 		}
