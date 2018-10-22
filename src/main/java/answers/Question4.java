0b962aadf0ebf70eff82ae723cc68610b625b57f
Charles-Tam <tamcheuklai@outlook.com>
2018-10-23 00:58:27 +0800

    Q4 edit

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -4,14 +4,12 @@ public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
 		// TODO Auto-generated method stub
-		int output=0, Machines;
+		int output=0;
 		if(numberMachines<rows.length){
-			Machines = rows[numberMachines].length;
+			int Machines = rows[numberMachines].length;
 			for(int i=0; i<Machines;i++){
-				if(!rows[numberMachines][i].equals("X")){
-				}else if(Character.isDigit(rows[numberMachines][i].charAt(0))){
+				if(Character.isDigit(rows[numberMachines][i].charAt(0))){
 					output += Integer.valueOf(rows[numberMachines][i]);
-				}else{
 				}
 			}
 		}
