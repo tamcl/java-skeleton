bdcb40970e357d15d81707d94a56d3fec193150d
Charles-Tam <tamcheuklai@outlook.com>
2018-10-24 06:10:38 +0800

    Q3

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -3,10 +3,17 @@ package answers;
 import helpers.Edge;
 
 public class Question3 {
-
+	static int[] check;
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
 		// TODO Auto-generated method stub
-		return -1;
+		check = new int[numNodes];
+		for(int i=0;i<numNodes;i++)check[i]=-1;
+		int output=0;
+		for(int i=0;i<edgeList.length;i++){
+			check[edgeList[i].getEdgeA()-1]=1;
+		}
+		for(int i=0;i<numNodes;i++)output+=check[i];
+		return output;
 	}
 
 }
