7875906cb57fc97dae8bfcd41e11ac783d9fe808
Charles-Tam <tamcheuklai@outlook.com>
2018-10-24 05:54:42 +0800

    Q2 single element edit

--- src/main/java/answers/Question2.java
+++ src/main/java/answers/Question2.java
@@ -1,8 +1,6 @@
 package answers;
 
 public class Question2 {
-	static int[] cashin;
-	static int[] cashout;
 	static int[][][] InComb;
 	static int[][][] OutComb;
 	static int[] In;
@@ -11,47 +9,44 @@ public class Question2 {
 	public static int equallyBalancedCashFlow(int[] cashflowIn, int[] cashflowOut) {
 		// TODO Auto-generated method stub
 		//global variable for in and out
-		cashin =new int[cashflowIn.length];
-		for(int i=0;i<cashflowIn.length;i++){
-			cashin[i] = cashflowIn[i];
-		}
-		cashout =new int[cashflowOut.length];
-		for(int i=0;i<cashflowOut.length;i++){
-			cashout[i] = cashflowOut[i];
-		}
 		//all the possible combination
 		int[] In = new int[combinationsSum(cashflowIn.length)];
 		int[] Out = new int[combinationsSum(cashflowOut.length)];
-		int output=1000;
+		int output=Integer.MAX_VALUE;
 		int count=0;
 		int maxIn, maxOut;
+		boolean InM, OutM;
 		maxIn=0;
-		for(int i=1; i<cashflowIn.length;i++){
-			if(combinations(cashflowIn.length,i)>maxIn){
-				maxIn=combinations(cashflowIn.length,i);
+		if(cashflowIn.length>1){
+			InM=true;
+			for(int i=1; i<cashflowIn.length;i++){
+				if(combinations(cashflowIn.length,i)>maxIn){
+					maxIn=combinations(cashflowIn.length,i);
+				}
 			}
+			InComb = new int[cashflowIn.length][maxIn][cashflowIn.length];
+		}else{
+			InM=false;
+			InComb = new int[1][1][1];
+			
 		}
-		InComb = new int[cashflowIn.length][maxIn][cashflowIn.length];
-		maxOut=0;
-		for(int i=1; i<cashflowOut.length;i++){
-			if(combinations(cashflowOut.length,i)>maxOut){
-				maxOut=combinations(cashflowOut.length,i);
+		if(cashflowOut.length>1){
+			OutM=true;
+			maxOut=0;
+			for(int i=1; i<cashflowOut.length;i++){
+				if(combinations(cashflowOut.length,i)>maxOut){
+					maxOut=combinations(cashflowOut.length,i);
+				}
 			}
+			OutComb = new int[cashflowOut.length][maxOut][cashflowOut.length];
+		}else{
+			OutM=false;
+			OutComb=new int[1][1][1];
 		}
-		OutComb = new int[cashflowOut.length][maxOut][cashflowOut.length];
+		
+		
+		
 		//set value in combination
-		for(int i=0;i<cashflowIn.length;i++){
-			for(int n=0;n<maxIn;n++){
-				for(int c=0; c<cashflowIn.length;c++)
-				InComb[i][n][c]=cashflowIn.length;
-			}
-		}
-		for(int i=0;i<cashflowOut.length;i++){
-			for(int n=0;n<maxOut;n++){
-				for(int c=0; c<cashflowOut.length;c++)
-				OutComb[i][n][c]=cashflowOut.length;
-			}
-		}	
 		for(int i=0;i<cashflowIn.length;i++){
 			for(int c=0;c<i+1;c++){
 				InComb[i][0][c]=c;
@@ -63,29 +58,31 @@ public class Question2 {
 			}
 		}
 		//in
-		for(int n=0;n<combinations(cashflowIn.length,1);n++){
-			InComb[0][n][0]=n;
-		}
-		for(int i=1;i<cashflowIn.length;i++){
-			for(int n=1;n<combinations(cashflowIn.length,i+1);n++){
-				for(int c=0;c<i+1;c++){
-					InComb[i][n][c]=InComb[i][n-1][c];
-				}
-				
-				InComb[i][n][i]=InComb[i][n][i]+1;
-				
-				if(InComb[i][n][i]>=cashflowIn.length){
-					int ct=0;
-					InComb[i][n][i]=InComb[i][n][i]-1;
-					for(int t=i;t>=1;t--){
-						if(InComb[i][n][t]-InComb[i][n][t-1]>1){
-							ct=t-1;
-							break;
-						}
+		if(cashflowIn.length>1){
+			for(int n=0;n<combinations(cashflowIn.length,1);n++){
+				InComb[0][n][0]=n;
+			}
+			for(int i=1;i<cashflowIn.length;i++){
+				for(int n=1;n<combinations(cashflowIn.length,i+1);n++){
+					for(int c=0;c<i+1;c++){
+						InComb[i][n][c]=InComb[i][n-1][c];
 					}
-					InComb[i][n][ct]=InComb[i][n][ct]+1;
-					for(int t=ct+1;t<=i;t++){
-						InComb[i][n][t]=InComb[i][n][t-1]+1;
+					
+					InComb[i][n][i]=InComb[i][n][i]+1;
+					
+					if(InComb[i][n][i]>=cashflowIn.length){
+						int ct=0;
+						InComb[i][n][i]=InComb[i][n][i]-1;
+						for(int t=i;t>=1;t--){
+							if(InComb[i][n][t]-InComb[i][n][t-1]>1){
+								ct=t-1;
+								break;
+							}
+						}
+						InComb[i][n][ct]=InComb[i][n][ct]+1;
+						for(int t=ct+1;t<=i;t++){
+							InComb[i][n][t]=InComb[i][n][t-1]+1;
+						}
 					}
 				}
 			}
@@ -94,29 +91,31 @@ public class Question2 {
 				
 
 		//out
-		for(int n=0;n<combinations(cashflowOut.length,1);n++){
-			OutComb[0][n][0]=n;
-		}
-		for(int i=1;i<cashflowOut.length;i++){
-			for(int n=1;n<combinations(cashflowOut.length,i+1);n++){
-				for(int c=0;c<i+1;c++){
-					OutComb[i][n][c]=OutComb[i][n-1][c];
-				}
-				
-				OutComb[i][n][i]=OutComb[i][n][i]+1;
-				
-				if(OutComb[i][n][i]>=cashflowOut.length){
-					int ct=0;
-					OutComb[i][n][i]=OutComb[i][n][i]-1;
-					for(int t=i;t>=1;t--){
-						if(OutComb[i][n][t]-OutComb[i][n][t-1]>1){
-							ct=t-1;
-							break;
-						}
+		if(cashflowOut.length>1){
+			for(int n=0;n<combinations(cashflowOut.length,1);n++){
+				OutComb[0][n][0]=n;
+			}
+			for(int i=1;i<cashflowOut.length;i++){
+				for(int n=1;n<combinations(cashflowOut.length,i+1);n++){
+					for(int c=0;c<i+1;c++){
+						OutComb[i][n][c]=OutComb[i][n-1][c];
 					}
-					OutComb[i][n][ct]=OutComb[i][n][ct]+1;
-					for(int t=ct+1;t<=i;t++){
-						OutComb[i][n][t]=OutComb[i][n][t-1]+1;
+					
+					OutComb[i][n][i]=OutComb[i][n][i]+1;
+					
+					if(OutComb[i][n][i]>=cashflowOut.length){
+						int ct=0;
+						OutComb[i][n][i]=OutComb[i][n][i]-1;
+						for(int t=i;t>=1;t--){
+							if(OutComb[i][n][t]-OutComb[i][n][t-1]>1){
+								ct=t-1;
+								break;
+							}
+						}
+						OutComb[i][n][ct]=OutComb[i][n][ct]+1;
+						for(int t=ct+1;t<=i;t++){
+							OutComb[i][n][t]=OutComb[i][n][t-1]+1;
+						}
 					}
 				}
 			}
@@ -143,14 +142,6 @@ public class Question2 {
 				ctt++;
 			}
 		}
-		
-		for(int i=0;i<In.length;i++){
-			for(int c=0; c<combinationsSum(cashflowOut.length);c++){
-				if(In[i]-Out[c]>=0){
-					output=In[i]-Out[c];
-				}
-			}
-		}
 		for(int i=0;i<In.length;i++){
 			for(int c=0; c<Out.length;c++){
 				if(In[i]-Out[c]<output&&In[i]-Out[c]>=0){
@@ -158,15 +149,6 @@ public class Question2 {
 				}
 			}
 		}
-
-		
-
-		
-		
-		
-		
-
-		
 		return output;
 	}
 	//sum of combination
@@ -186,9 +168,4 @@ public class Question2 {
 		for(int count =1;count<=n;count++)fact=fact*count;
 		return fact;
 	}
-	//tbc
-	public static int remaindersum(int[] cashflow, int number){
-		return -1;
-	}
-
 }
